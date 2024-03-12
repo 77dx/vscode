ca1010f40a6da34f31e26095b142c2c397e195c6
77dx <396321556@qq.com>
2024-03-12 16:06:53 +0800

    Hello World

---
+++ HelloWorld.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello World!\n");
+    // printf("%d\n",sizeof(char));
+    // printf("%d\n",sizeof(short));
+    // printf("%d\n",sizeof(int));
+    // printf("%d\n",sizeof(long));
+    // printf("%d\n",sizeof(long long));
+    // printf("%d\n",sizeof(float));
+    // printf("%d\n",sizeof(double));
+    int a,b,sum;
+    scanf("%d %d",&a,&b);
+    sum = a+b;
+    printf("sum=%d\n",sum);
+    return sum;
+}
